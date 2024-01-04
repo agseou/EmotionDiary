f719a3dc84df8d26a587214419c3b185b734246e
SeoU <tjdnfkd1225@naver.com>
2024-01-05 02:07:58 +0900

    Count값 UserDefaults로 변환 & 전체 리셋 버튼 추가

--- EmotionDiary/ViewController.swift
+++ EmotionDiary/ViewController.swift
@@ -12,7 +12,16 @@ class ViewController: UIViewController {
     @IBOutlet var slimes: [UIButton]!
     @IBOutlet var slimesLable: [UILabel]!
     
-    var slimesCount: [Int] = [0,0,0,0,0,0,0,0,0]
+    //UserDefault로 배열 구성하기
+    var slimesCount: [Int] = [UserDefaults.standard.integer(forKey: "Idx0"),
+                              UserDefaults.standard.integer(forKey: "Idx1"),
+                              UserDefaults.standard.integer(forKey: "Idx2"),
+                              UserDefaults.standard.integer(forKey: "Idx3"),
+                              UserDefaults.standard.integer(forKey: "Idx4"),
+                              UserDefaults.standard.integer(forKey: "Idx5"),
+                              UserDefaults.standard.integer(forKey: "Idx6"),
+                              UserDefaults.standard.integer(forKey: "Idx7"),
+                              UserDefaults.standard.integer(forKey: "Idx8")]
     
     var slimesEmotion: [String] = ["행복해", "사랑해", "행복해", "당황해", "속상해", "우울해", "심심해", "행복해", "행복해"]
     
@@ -35,11 +44,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapslime(_ sender: UIButton) {
-        print("\(sender.tag)번째 슬라임 누럿더")
-        slimesCount[sender.tag] += 1
-        slimesLable[sender.tag].text = "\(slimesEmotion[sender.tag])  \(slimesCount[sender.tag])"
+        
+        print("\(slimesCount[sender.tag]) \(sender.tag)번째 슬라임 누럿더")
+        
+        let num = UserDefaults.standard.integer(forKey: "Idx\(sender.tag)")
+        
+        let newnum = num + 1
+        
+        UserDefaults.standard.set(newnum, forKey: "Idx\(sender.tag)")
+        
+        print("\(slimesCount[sender.tag])")
+        
+        slimesLable[sender.tag].text = "\(slimesEmotion[sender.tag])  \(UserDefaults.standard.integer(forKey: "Idx\(sender.tag)"))"
+        
     }
     
+    //전체 리셋버튼
+    @IBAction func tapResetBtn(_ sender: UIButton) {
+        
+        for i in 0...8 {
+            UserDefaults.standard.set(0, forKey: "Idx\(i)")
+            slimesLable[i].text = "\(slimesEmotion[i])  \(UserDefaults.standard.integer(forKey: "Idx\(i)"))"
+        }
+    }
     
     @objc func leftBarButtonItemClicked(){
         print("leftBarButton Item Clicked")
